14740a8fe8f6957d98ffdf2f898837358a5d2fb5
tzlilhaim <tzlilhaim480@gmail.com>
2020-08-20 23:16:06 +0300

    Added challenging & extensions exercises

--- day-4/jQuery101/main.js
+++ day-4/jQuery101/main.js
@@ -94,6 +94,110 @@ $("#exercise-1-body").on("click", "button", function () {
 })
 
 // Exercise 2
-$("#exercise-1-body").on("click","li",function(){
+$("#exercise-1-body").on("click", "li", function () {
     $(this).remove()
+})
+
+// Exercise 3
+// Create the 2 boxes and append to body
+$("#exercise-3-body").append($(`<div class='box-exercise-3'></div>`))
+$("#exercise-3-body").append($(`<div class='box-exercise-3'></div>`))
+
+// On hover listener
+$(".box-exercise-3").hover(function () {
+    $(this).siblings(".box-exercise-3").css("background-color", "red")
+    $(this).css("background-color", "purple")
+})
+
+// Exercise 4 & Extension
+const getItemInCartCount = (itemText) => {
+    const cartItemsArr = $("#cart").find(".cart-item").toArray()
+
+    let itemCount = 0
+    let i = 0
+    while (!itemCount && i < cartItemsArr.length) {
+        if ($(cartItemsArr[i]).text().includes(itemText)) {
+
+            // Get quantity of this item in the cart
+            if ($(cartItemsArr[i]).text().includes("x")) {
+                const xIndex = $(cartItemsArr[i]).text().indexOf("x")
+                itemCount = parseInt($(cartItemsArr[i]).text().slice(xIndex + 1))
+            } else {
+                itemCount++
+            }
+        } else {
+            i++
+        }
+    }
+    return itemCount
+}
+
+$(".item").on("click", function () {
+    if ($(this).data().instock) {
+        
+        // Get quantity of this item in the cart
+        const itemCount = getItemInCartCount($(this).text())
+
+        if (itemCount) {
+            const item = $(`div:contains(${$(this).text()}).cart-item`)
+
+            // change text
+            $(item).text(`${$(this).text()} x${itemCount + 1}`)
+        } else {
+
+            // If doesn't exist yet, create this element and append to cart
+            $("#cart").append(`<div class='cart-item'>${$(this).text()}</div>`)
+        }
+    }
+})
+
+// Remove item from cart on click
+$("#cart").on("click", ".cart-item", function () {
+
+    // Get quantity of this item in the cart
+    const itemCount = getItemInCartCount($(this).text())
+
+    if (itemCount == 1) {
+
+        // If there's only one, remove altogether
+        $(this).remove()
+    } else {
+        const xIndex = $(this).text().indexOf("x")
+        const itemName = $(this).text().slice(0, xIndex - 1)
+        if (itemCount == 2) {
+
+            // If there are two, change text - remove count
+            $(this).text(itemName)
+        } else {
+
+            // If there are more than 2 items, decrease amounnt by 1
+            $(this).text(`${itemName} x${itemCount - 1}`)
+        }
+    }
+})
+
+// Exercise 5
+const fruits = [
+    { name: "Orange", color: "orange" },
+    { name: "Banana", color: "yellow" },
+    { name: "Coconut", color: "brown" },
+    { name: "Kiwi", color: "brown" },
+    { name: "Lemon", color: "yellow" },
+    { name: "Cucumber", color: "green" },
+    { name: "Persimmon", color: "orange" },
+    { name: "Pumpkin", color: "orange" }
+]
+
+for (fruit of fruits) {
+    $("#basket").append(`<div class='${fruit.color}'>${fruit.name}</div>`)
+}
+
+// Extension - Color Picker
+for (color of $("#colors").children().toArray()) {
+    $(color).addClass("picker")
+    $(color).css("background-color", $(color).text().toLowerCase())
+}
+
+$(".picker").on("click", function () {
+    $(".box-exercise-6").css("background-color", $(this).data().color)
 })
